dded1a5590db56077d3026251236dc6c3d373762
HwangHyeJin <h960502hhj@gmail.com>
2022-07-12 08:39:49 +0900

    style/strength css 수정

--- src/components/strength/Strength.jsx
+++ src/components/strength/Strength.jsx
@@ -1,42 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import gsap from "gsap";
 
 const Strength = ({ skills }) => {
-  const [content, setContent] = useState(skills[0].contents.map((content, idx) => (
-    <div key={idx.toString()}>
-      {content}
-    </div>
-  )));
+  const [content, setContent] = useState(
+    skills[0].contents.map((content, idx) => (
+      <div key={idx.toString()}>{content}</div>
+    ))
+  );
 
   const setHovalItem = (e) => {
     let tl = gsap.timeline();
     tl.to(e.target, { y: -40, duration: 0.5 });
     tl.to(e.target, { y: 0, duration: 1 });
 
-    const contents = skills.filter(skill => skill.key === e.target.id)[0].contents;
+    const contents = skills.filter((skill) => skill.key === e.target.id)[0]
+      .contents;
     setContent(
-      contents.map((content, idx) => (
-        <div key={idx.toString()}>
-          {content}
-        </div>
-      )));
+      contents.map((content, idx) => <div key={idx.toString()}>{content}</div>)
+    );
   };
 
   return (
     <>
       <div className="strength-wrapper">
         <ul>
-          <li>
+          <li className="skillList">
             {skills.map((skill, idx) => (
-              <div id={skill.key} key={idx.toString()} className={skill.key === "react" ? "skill skill-1" : "skill"}>
-                <h1 id={skill.key} onMouseOver={setHovalItem}>{skill.title}</h1>
+              <div id={skill.key} key={idx.toString()} className="skill">
+                <h1 id={skill.key} onMouseOver={setHovalItem}>
+                  {skill.title}
+                </h1>
               </div>
             ))}
           </li>
           <li>
-            <div className="skill-content">
-              {content}
-            </div>
+            <div className="skill-content">{content}</div>
           </li>
         </ul>
       </div>
--- src/components/strength/index.jsx
+++ src/components/strength/index.jsx
@@ -5,19 +5,17 @@ import "./Strength.css";
 const Scene3 = () => {
   return (
     <>
-    <div className="section-0-frame grid-paper">
+      <div className="section-0-frame grid-paper">
         <div className="description">
-            <h2 className="title shadow">
-                Strength.
-                <div className="pin"></div>
-            </h2>
+          <h2 className="title shadow">
+            Strength.
+            <div className="pin"></div>
+          </h2>
         </div>
-    </div>
-    <div className="strength-wrapper">
-        <ul>
-            <Strength skills={skills}/>
-        </ul>
-    </div>
+      </div>
+      <div className="strength-wrapper">
+        <Strength skills={skills} />
+      </div>
     </>
   );
 };
