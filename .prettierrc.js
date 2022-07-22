249bf0cb10a9baade126beb71a6db986647b846f
Hwang-gPwls <h960502hhj@gmail.com>
2022-07-22 12:15:01 +0900

    wrapper-description

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+/**
+ * @name: .pretteierrc.js
+ * @description: Prettier는 코드를 읽어들여서 사용자 옵션에 따라 코드를 다시 포맷팅하는 "코드 포맷터" 입니다.
+ * @version: 2.0
+ */
+
+module.exports = {
+	printWidth: 80,
+	tabWidth: 1,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	trailingComma: 'all',
+	jsxBracketSameLine: true,
+	bracketSpacing: true,
+	arrowParens: 'always',
+};
